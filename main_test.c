1ddc2225bb4910bdd84e96359bb3cf4b626d9ba7
Thomas Gozalie <114067350+regencode@users.noreply.github.com>
2022-12-01 17:56:23 +0700

    Add files via upload

--- main_test.c
+++ main_test.c
@@ -1,79 +1,16 @@
 
 #include <stdio.h>
-#include <time.h>
 #include "mylib/mylib.h"
      
-clock_t start, end;
-double cpu_time_used;
-double total_time_used;
-int res;
-
-FILE *fp, *fp2;
-
+int n;
 
 int main(){
+    printf("Enter n: ");
+    scanf("%d", &n);
 
+    printf("Iterative fib result: %d\n", fibIterative(n));
+    printf("Recursive fib result: %d\n", fibRecursive(n));
 
-    //Iterative
-
-    fp = fopen("dataIterative.dat", "w");
-    fp2 = fopen("dataIterativeMagnified.dat", "w");
-
-    for(int i=1; i<=50000; i++){
-
-        if(i<=40){
-
-            start = clock();
-            res = fibIterative(i);
-            end = clock();
-
-            cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-            total_time_used += cpu_time_used;
-
-            fprintf(fp, "(%d,%lf),", i, cpu_time_used); //Iterative - Actual Runtime
-
-            printf("Iterative Fib Total Runtime: %lfs   Index: %d\n", total_time_used, i);
-
-        } else if(i%1000==0){
-            start = clock();
-            res = fibIterative(i);
-            end = clock();
-
-            cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-            total_time_used += cpu_time_used;
-
-            fprintf(fp2, "(%d,%lf),", i/1000, cpu_time_used*100000); //Iterative - Magnified Runtime per 1000 steps
-
-            printf("Iterative Fib Total Runtime: %lfs   Index: %d\n", total_time_used, i);
-        }
-
-    }
-    printf("Iterative Fib data generated at dataIterative.dat and dataIterativeMagnified.dat\n");
-    fclose(fp);
-    fclose(fp2);
-
-
-    //Recursive
-
-    fp = fopen("dataRecursive.dat", "w");
-    fp2 = fopen("dataRecursiveMagnified.dat" , "w");
-
-    for(int i=1; i<=40; i++){
-        start = clock();
-        res = fibRecursive(i);
-        end = clock();
-
-        cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-        total_time_used += cpu_time_used;
-
-        fprintf(fp, "(%d,%lf),", i, cpu_time_used); //Recursive - Actual Runtime
-        fprintf(fp2, "(%d,%lf),", i, cpu_time_used*100); // Recursive - Magnified Runtime
-
-        printf("Recursive Fib Total Runtime: %lfs   Index: %d\n", total_time_used, i);
-    }
-    printf("Recursive Fib data generated at dataRecursive.dat and dataRecursiveMagnified.dat\n");
-    fclose(fp);
-    fclose(fp2);
 
     return 0;
 }
